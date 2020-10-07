e4091ee340f41651ca05d0250b8b99b45cc2b103
Roland Schmitt <rschmitt@samhammer.de>
2020-10-07 16:17:23 +0200

    initial project

---
+++ src/TrxToHtml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrxToHtml
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
