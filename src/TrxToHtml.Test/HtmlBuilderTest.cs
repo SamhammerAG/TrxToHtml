ab0eac7c2e2634685097ce6920dfa12f66b23bc9
Roland Schmitt <rschmitt@samhammer.de>
2020-10-08 12:22:04 +0200

    add html template parsing

---
+++ src/TrxToHtml.Test/HtmlBuilderTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using TrxToHtml.Models;
+using TrxToHtml.Models.Results;
+using Xunit;
+
+namespace TrxToHtml.Test
+{
+    public class HtmlBuilderTest
+    {
+        [Fact]
+        public void GetHtml_ShouldParseTestRunPlaceholders()
+        {
+            var textTemplate = "{{ for run in TestRuns }}{{ for result in run.Results }}{{ result.testName }};{{ end }}{{ end }}";
+
+            var testRuns = new List<TestRun>
+            {
+                new TestRun
+                {
+                    Results = new[]
+                    {
+                        new UnitTestResult { testName = "test1" },
+                        new UnitTestResult { testName = "test2" }
+                    }
+                }
+            };
+
+            var html = new HtmlBuilder().ParseHtml(testRuns, textTemplate);
+
+            html.Should().Be("test1;test2;");
+        }
+    }
+}
--- src/TrxToHtml/HtmlBuilder.cs
+++ src/TrxToHtml/HtmlBuilder.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+using Scriban;
 using TrxToHtml.Models;
 
 namespace TrxToHtml
 {
     public class HtmlBuilder
     {
+        public string GetEmbeddedTemplate(string filename = "template.html")
+        {
+            var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+            var embeddedFile = embeddedProvider.GetFileInfo(filename);
+            using var stream = embeddedFile.CreateReadStream();
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+
         public string GetHtml(List<TestRun> testRuns)
         {
-            // TODO generate html
-            return testRuns.FirstOrDefault()?.Results.FirstOrDefault()?.testName;
+            var textTemplate = GetEmbeddedTemplate();
+            return ParseHtml(testRuns, textTemplate);
+        }
+
+        public string ParseHtml(List<TestRun> testRuns, string textTemplate)
+        {
+            var template = Template.Parse(textTemplate);
+            return template.Render(new { TestRuns = testRuns }, member => member.Name);
         }
     }
 }
