4bab38748d623c422a133c838a438708389dc3fe
Roland Schmitt <rschmitt@samhammer.de>
2020-10-08 14:42:05 +0200

    add script to install tool from local project

---
+++ src/install-tool.ps1
@@ -0,0 +1,7 @@
+﻿function Get-ScriptDirectory {
+    Split-Path -parent $PSCommandPath
+}
+
+dotnet pack -c Release /p:Version=0.0.0
+dotnet tool uninstall -g TrxToHtml
+dotnet tool install -g --add-source "$(Get-ScriptDirectory)/TrxToHtml/bin/Release" --version 0.0.0 TrxToHtml
